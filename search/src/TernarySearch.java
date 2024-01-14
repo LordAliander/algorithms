4457576de124f4629d5eddcbc8d13d25f1086993
LordAliander <82032168+LordAliander@users.noreply.github.com>
2024-01-14 14:21:18 +0100

    changed ternary search to be recursive rather than use a loop

--- search/src/TernarySearch.java
+++ search/src/TernarySearch.java
@@ -1,28 +1,21 @@
-
 public class TernarySearch {
 
-    static int ternarySearch(int[] array, int n) {
-        int left = 0;
-        int right = array.length - 1;
-
-        while (left <= right) {
+    static int ternarySearch(int[] array, int n, int left, int right) {
+        if (left <= right) {
             int mid1 = left + (right - left) / 3;
             int mid2 = right - (right - left) / 3;
 
-            if (n == array[mid1]) return mid1;
+            if (n == array[mid1]) return mid1; // Element found
             else if (n == array[mid2]) return mid2;
-            else if (n < array[mid1]) right = mid1 - 1;
-            else if (n > array[mid2]) left = mid2 + 1;
-            else {
-                left = mid1 + 1;
-                right = mid2 - 1;
-            }
+            else if (n < array[mid1]) return ternarySearch(array, n, left, mid1 - 1);
+            else if (n > array[mid2]) return ternarySearch(array, n, mid2 + 1, right);
+            else return ternarySearch(array, n, mid1 + 1, mid2 - 1);
         }
         return -1; // Element not found
     }
 
     static int search(int[] array, int n) {
-        return ternarySearch(array, n);
+        return ternarySearch(array, n, 0, array.length - 1);
     }
 
 }
