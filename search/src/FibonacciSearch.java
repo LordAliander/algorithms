15aed1f4c806ae4e98d6d45c097f18e8e380b4af
LordAliander <82032168+lordaliander@users.noreply.github.com>
2024-01-14 11:09:50 +0100

    Added a dynamic solution for finding fibonacci numbers.

--- search/src/FibonacciSearch.java
+++ search/src/FibonacciSearch.java
@@ -9,11 +9,36 @@
  * 4. If not found repeat until index >= 0
  */
 
+import java.util.HashMap;
 
 public class FibonacciSearch {
-    static int search(int[] array, int n) {
-        int length = array.length;
+    /*
+     * In this case the HashMap is used as a data structure analog to a dictionary in python.
+     * If the program requests the same fibonacci number more than once, it can retrieve it
+     * by accessing the value at the key, which was stored previously.
+     */
+    static HashMap<Integer, Integer> memo = new HashMap<>();
 
-        return 3;
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        if (memo.containsKey(n)) { // dynamic element
+            return memo.get(n);
+        }
+
+        int result = fibonacci(n - 1) + fibonacci(n - 2);
+        memo.put(n, result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {2, 5, 8, 10, 12, 15, 18, 20, 23, 25, 28, 30, 32, 35, 38,
+                40, 43, 46, 48, 50, 53, 56, 58, 61, 64};
+        int n = 40;
+
+
     }
 }
