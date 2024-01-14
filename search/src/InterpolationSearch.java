24b8735a54e8b19e64d4598b9a20172bcb16a478
LordAliander <82032168+LordAliander@users.noreply.github.com>
2024-01-14 14:30:38 +0100

    setup for next program

--- search/src/InterpolationSearch.java
+++ search/src/InterpolationSearch.java
@@ -1,2 +1,12 @@
 public class InterpolationSearch {
+
+    static int interpolationSearch() {
+        return 1;
+    }
+
+    static int search(int[] array, int n) {
+
+        return interpolationSearch();
+    }
+
 }
