8f952c79eb44e2eb24d039da4cd9194ae8ba809a
LordAliander <82032168+LordAliander@users.noreply.github.com>
2024-01-14 14:06:34 +0100

    finished fibonacci search and updated the main file

--- search/src/FibonacciSearch.java
+++ search/src/FibonacciSearch.java
@@ -1,44 +1,46 @@
-
-/*
- * 1. Find a fibonacci number >= length of array
- * 2. Find the two previous fibonacci numbers. One is the offset, the other one the index.
- * 3. Compare n to the element at index.
- *  3.1 If n == elem at index return index.
- *  3.2 If n < elem at index -> search left of index
- *  3.3 If n > elem at index -> search right of index
- * 4. If not found repeat until index >= 0
- */
-
-import java.util.HashMap;
-
 public class FibonacciSearch {
-    /*
-     * In this case the HashMap is used as a data structure analog to a dictionary in python.
-     * If the program requests the same fibonacci number more than once, it can retrieve it
-     * by accessing the value at the key, which was stored previously.
-     */
-    static HashMap<Integer, Integer> memo = new HashMap<>();
+    static int fibonacciSearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
 
-    static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
+        int fibMMinus2 = 0;
+        int fibMMinus1 = 1;
+        int fibM = fibMMinus1 + fibMMinus2;
+
+        while (fibM < right - left + 1) { // creating the fibonacci numbers
+            fibMMinus2 = fibMMinus1;
+            fibMMinus1 = fibM;
+            fibM = fibMMinus1 + fibMMinus2;
         }
 
-        if (memo.containsKey(n)) { // dynamic element
-            return memo.get(n);
+        int offset = -1;
+
+        while (fibM > 1) {
+            int i = Math.min(offset + fibMMinus2, right - 1);
+
+            if (array[i] < target) {
+                fibM = fibMMinus1;
+                fibMMinus1 = fibMMinus2;
+                fibMMinus2 = fibM - fibMMinus1;
+                offset = i;
+            } else if (array[i] > target) {
+                fibM = fibMMinus2;
+                fibMMinus1 = fibMMinus1 - fibMMinus2;
+                fibMMinus2 = fibM - fibMMinus1;
+            } else {
+                return i;
+            }
         }
 
-        int result = fibonacci(n - 1) + fibonacci(n - 2);
-        memo.put(n, result);
-        return result;
+        if (fibMMinus1 == 1 && array[offset + 1] == target) {
+            return offset + 1;
+        }
+
+        return -1;
     }
 
-    public static void main(String[] args) {
-
-        int[] array = {2, 5, 8, 10, 12, 15, 18, 20, 23, 25, 28, 30, 32, 35, 38,
-                40, 43, 46, 48, 50, 53, 56, 58, 61, 64};
-        int n = 40;
-
-
+    static int search(int[] array, int n) {
+        return fibonacciSearch(array, n);
     }
+
 }
--- search/src/Main.java
+++ search/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int[] array = {2, 5, 8, 10, 12, 15, 18, 20, 23, 25, 28, 30, 32, 35, 38,
                         40, 43, 46, 48, 50, 53, 56, 58, 61, 64};
         int n = 53;
-        System.out.println("Index of " + 53 + " = " + BinarySearch.search(array, n));
+        System.out.println("Index of " + 53 + " = " + FibonacciSearch.search(array, n));
 
 
     }
