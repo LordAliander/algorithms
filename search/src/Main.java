2b4f1ccc8423defeadf8d5a5e5328d5421283a3f
LordAliander <82032168+LordAliander@users.noreply.github.com>
2024-01-14 14:18:20 +0100

    added ternary search

--- search/src/Main.java
+++ search/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
         int[] array = {2, 5, 8, 10, 12, 15, 18, 20, 23, 25, 28, 30, 32, 35, 38,
                         40, 43, 46, 48, 50, 53, 56, 58, 61, 64};
-        int n = 53;
-        System.out.println("Index of " + 53 + " = " + FibonacciSearch.search(array, n));
+        int n = 50;
+        System.out.println("Index of " + n + " = " + TernarySearch.search(array, n));
 
 
     }
--- search/src/TernarySearch.java
+++ search/src/TernarySearch.java
@@ -1,2 +1,28 @@
+
 public class TernarySearch {
+
+    static int ternarySearch(int[] array, int n) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid1 = left + (right - left) / 3;
+            int mid2 = right - (right - left) / 3;
+
+            if (n == array[mid1]) return mid1;
+            else if (n == array[mid2]) return mid2;
+            else if (n < array[mid1]) right = mid1 - 1;
+            else if (n > array[mid2]) left = mid2 + 1;
+            else {
+                left = mid1 + 1;
+                right = mid2 - 1;
+            }
+        }
+        return -1; // Element not found
+    }
+
+    static int search(int[] array, int n) {
+        return ternarySearch(array, n);
+    }
+
 }
